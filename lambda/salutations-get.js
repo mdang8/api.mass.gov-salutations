3de1c044d0ecc710b08a0d74e8b5632262389afc
mdang8 <mdang808@gmail.com>
2016-08-10 11:33:13 -0400

    updated lambda functions

--- lambda/salutations-get.js
+++ lambda/salutations-get.js
@@ -3,117 +3,152 @@ let https = require('https');
 
 exports.handler = (event, context, callback) => {
 
-    var request = https.get('https://s3.amazonaws.com/salutations-data.api.mass.gov/salutations-data.json',
-
-
+    // makes a GET request to get the JSON file from the S3 bucket
+    var request = https.get(
+        "https://s3.amazonaws.com/salutations-data.api.mass.gov/salutations-data.json",
         function(response) {
             var body = '';
 
-            response.on('data', function(chunk) {
+            // accepts data stream and adds to body variable
+            response.on("data", function(chunk) {
                 body += chunk;
             });
 
-            response.on('end', function() {
-                body = JSON.parse(body).salutationsData;
+            // triggers when data stream is complete
+            response.on("end", function() {
+                try {
+                    // parses body variable data into object array
+                    body = JSON.parse(body).salutationsData;
+                } catch (err) {
+                    // returns fail
+                    return context.fail(
+                        "Error with parsing JSON body: " +
+                        err);
+                }
+
+                // calls getData function with body variable as argument
                 getData(body);
             });
-        });
+        }); // end of request
 
+    // determines what data to return
     var getData = function(data) {
-        var id = event.id !== undefined ? event.id : '';
         var name = event.name !== undefined ? event.name : '';
-        var greeting = event.greeting !== undefined ? event.greeting : '';
+        var greeting = event.greeting !== undefined ? event.greeting :
+            '';
         var gender = event.gender !== undefined ? event.gender : '';
 
+        // the list to be returned
         var salutationsList = [];
+
         var salutationsListGreeting = [];
         var salutationsListGender = [];
 
-        if (id === '' && name === '' && greeting === '' && gender === '') {
-            console.log("Return all from mock.");
+        // if none of the parameters are specified, return all
+        if (name === '' && greeting === '' && gender === '') {
+            console.log("Return all.");
             salutationsList = data;
 
+            // exits lambda function, returns successful data
             return context.done(null, salutationsList);
         }
 
-        if (id !== '') {
-            data.forEach(function (obj) {
-                if (obj.id.search(new RegExp(id, "i")) > -1) {
-                    salutationsList.push(obj);
-                }
-            });
-
-            return context.done(null, salutationsList);
-        }
-
+        // if name is specified
         if (name !== '') {
-            data.forEach(function (obj) {
-                if (obj.name.search(new RegExp(name, "i")) > -1) {
+            // for each object in the JSON data
+            data.forEach(function(obj) {
+                // compares the input name with the object name
+                if (obj.name === name) {
+                    // adds the object to the list to be returned
                     salutationsList.push(obj);
                 }
             });
 
+            // if the return list has objects and the greeting is also specified
             if (salutationsList.length > 0 && greeting !== '') {
-                salutationsList.forEach(function (obj) {
+                // for each object currently in the return list
+                salutationsList.forEach(function(obj) {
+                    // compares the input greeting with the object greeting
                     if (obj.greeting === greeting) {
+                        // adds the object to the greetings list
                         salutationsListGreeting.push(obj);
                     }
                 });
 
+                // updates the new greetings list to be the list returned
                 salutationsList = salutationsListGreeting;
             }
 
+            // if the return list has objects and the gender is also specified
             if (salutationsList.length > 0 && gender !== '') {
-                salutationsList.forEach(function (obj) {
+                // for each object currently in the return list
+                salutationsList.forEach(function(obj) {
+                    // compares the input gender with the object gender
                     if (obj.gender === gender) {
+                        // adds the object to the gender list
                         salutationsListGender.push(obj);
                     }
                 });
 
+                // updates the new genders list to be the list returned
                 salutationsList = salutationsListGender;
             }
 
-            return context.done(null, salutationsList);
-        }
-
-        if (greeting !== '') {
-            data.forEach(function (obj) {
+            // if greeting is specified
+        } else if (greeting !== '') {
+            // for each object in the JSON data
+            data.forEach(function(obj) {
+                // compares the input greeting with the object greeting
                 if (obj.greeting === greeting) {
+                    // adds the object to the list to be returned
                     salutationsList.push(obj);
                 }
             });
 
+            // if the return list has objects and the gender is also specified
             if (salutationsList.length > 0 && gender !== '') {
-                salutationsList.forEach(function (obj) {
+                // for each object currently in the return list
+                salutationsList.forEach(function(obj) {
+                    // compares the input gender with the object gender
                     if (obj.gender === gender) {
-                        salutationsList.push(obj);
+                        // adds the object to the gender list
+                        salutationsListGender.push(obj);
                     }
                 });
 
+                // updates the new gender list to be the list returned
                 salutationsList = salutationsListGender;
             }
 
-            return context.done(null, salutationsList);
-        }
-
-        if (gender !== '') {
-            data.forEach(function (obj) {
+            // if gender is specified
+        } else if (gender !== '') {
+            // for each object in the JSON data
+            data.forEach(function(obj) {
+                // compares the input gender with the object gender
                 if (obj.gender === gender) {
+                    // adds the object to the list to be returned
                     salutationsList.push(obj);
                 }
             });
 
+            // if the return list has objects and the greeting is also specified
             if (salutationsList.length > 0 && greeting !== '') {
-                salutationsList.forEach(function (obj) {
+                // for each object currently in the return list
+                salutationsList.forEach(function(obj) {
+                    // compares the input greeting with the object greeting
                     if (obj.greeting === greeting) {
-                        salutationsList.push(obj);
+                        // adds the object to the greeting list
+                        salutationsListGreeting.push(obj);
                     }
                 });
 
+                // updates the new greeting list to be the list returned
                 salutationsList = salutationsListGreeting;
             }
         }
 
+        // exits lambda function, returns successful data
         return context.done(null, salutationsList);
-    }
-};
+    } // end of getData
+
+}; // end of exports handler
--- lambda/salutations-post.js
+++ lambda/salutations-post.js
@@ -4,77 +4,101 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'us-east-1';
 var fs = require('fs');
 
-exports.handler = (event, context, callback) => {
-    var request = https.get("https://s3.amazonaws.com/salutations-data.api.mass.gov/salutations-data.json",
-        function(response) {
-            var body = '';
+exports.handler = (event, context) => {
 
-            response.on("data", function(chunk) {
-                body += chunk;
+    // makes a GET request to get the JSON file from the S3 bucket
+    // the getData function takes in a callback function as an argument
+    var getData = function(callback) {
+        https.get("https://s3.amazonaws.com/salutations-data.api.mass.gov/salutations-data.json",
+            function(response) {
+                var body = '';
+
+                // accepts data stream and adds to body variable
+                response.on("data", function(chunk) {
+                    body += chunk;
+                });
+
+                // triggers when data stream is complete
+                response.on("end", function() {
+                    try {
+                        // parses body variable data into object array
+                        body = JSON.parse(body).salutationsData;
+
+                        // calls the callback function once the data is ready
+                        callback(body);
+                    } catch(err) {
+                        // returns fail
+                        return context.fail("Error with parsing JSON body: " + err);
+                    }
+                });
             });
+    };  // end of getData
 
-            response.on("end", function() {
-                try {
-                    body = JSON.parse(body);
-                } catch(err) {
-                    console.error("Error with parsing JSON body.");
-                    return;
-                }
-
-                addRecord(body);
-            });
-        });
-
+    // adds a new record to the JSON data
     var addRecord = function(data) {
-        var id = data['salutationsData'].length;  // index starts at 0
+        var id = data.length;  // index starts at 0
         var name = event.name !== undefined ? event.name : '';
         var greeting = event.greeting !== undefined ? event.greeting : '';
         var gender = event.gender !== undefined ? event.gender : '';
         var message = event.message !== undefined ? event.message : '';
 
+        // the new record object to add with the parameters from the event
         var newRecord = {
             "id": id.toString(),
             "name": name,
             "greeting": greeting,
             "gender": gender,
             "message": message
-        }
+        };
 
-        data['salutationsData'].push(newRecord);
-        writeData(JSON.stringify(data));
-        console.log(data);
-    }
+        // adds the new record to the JSON array
+        data.push(newRecord);
 
-    var writeData = function(data) {
-        fs.writeFile("/tmp/salutations-data.json", data, function(err) {
-            if(err)
-                context.fail("writeFile failed: " + err);
-            else
-                console.log("writeFile succeeded");
-        });
+        // calls the writeData function with a string of the JSON data as the argument
+        uploadData(JSON.stringify(data));
+    };  // end of addRecord
 
-        uploadData(data);
-    }
+    // uploads the JSON file to the S3 bucket
+    var uploadData = function(body) {
+        body = "{ \"salutationsData\":\n\n" + body + "\n\n}";
 
-    var uploadData = function(data) {
-        var body = fs.readFile("/tmp/salutations-data.json");
+        // s3 parameters for uploading
         var s3obj = new AWS.S3({
             params: {
                 Bucket: "salutations-data.api.mass.gov",
-                Key: "salutations-data.json"
+                Key: "salutations-data.json",
+                //ContentType: "text/html",
+                Body: body
             }
         });
 
+        // try-catch for uploading errors
         try {
-            s3obj.upload({Body: data}).on('httpUploadProgress', function(evt) {
+            // uploads the modified data
+            s3obj.upload().on('httpUploadProgress', function(evt) {
                 console.log(evt);
             }).send(function(err, data) {
-                console.log(err, data)
+                console.log(err, data);
             });
-        } catch (err) {
-            context.fail("Error on upload.");
-        }
 
-        context.done(null, data);
-    }
-};
+            // waits one second before making another GET request in case of latency
+            setTimeout(function() {
+                // calls the getData function with the showUpdates function as the callback
+                getData(showUpdates);
+            }, 1000);
+
+        } catch (err) {
+            // exits lambda fucntion, returns fail
+            return context.fail("Error on upload: " + err);
+        }
+    };  // end of uploadData
+
+    // returns the new data to show the changes and exits the lambda function
+    var showUpdates = function(data) {
+        return context.done(null, data);
+    };  // end of showUpdates
+
+    // initializes the program by calling the getData function with the addRecord function as the callback
+    getData(addRecord);
+
+};  // end of exports handler
